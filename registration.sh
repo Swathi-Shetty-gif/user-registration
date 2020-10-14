4827845303b658652c9ac256ba536a0dbd6e940b
Swathi-Shetty-gif <swathishetty9924@gmail.com>
2020-10-14 22:03:38 +0530

    added:special case

--- registration.sh
+++ registration.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "user registration system ensureing all validation "
-read -p "enter a password with a alpha numeric " numpass
-pat='^(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9]{8,}'
-if [ $numpass =~ $pat ]
+read -p "enter a password with a special case " spepass
+pat='^(?=.*[A-Z])(?=.*[0-9])(?=.*[!-+$@])[A-Za-z0-9!-+$@]{8,}$'
+if [ $spepass =~ $pat ]
 then
 echo "valid"
 else
