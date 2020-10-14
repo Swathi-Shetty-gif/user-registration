25ee598fb58adb4f62ee3e57fbf32196da8a6e65
Swathi-Shetty-gif <swathishetty9924@gmail.com>
2020-10-14 18:06:10 +0530

    added: email

--- registration.sh
+++ registration.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 echo "user registration system ensureing all validation "
-read -p "enter a last name" lname
-pat='^([A-Z]{1}[a-z]{2,})+.([A-Z]{1}[a-z]{2,})+$'
-if [ $lname =~ $pat ]
+read -p "enter a email " email
+pat='^[a-zA-Z0-9]+([._+][a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-z]{2,3}([.][a-z]{2})*$'
+if [ $email =~ $pat ]
 then
 echo "valid"
 else
  echo "not valid "
 fi
+#^[a-zA-Z0-9]+([.+-_*][a-zA-Z0-]+)*@[a-zA-Z0-9]+[a-z]{2,4}([.][a-z]{2})*$
