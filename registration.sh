94ca481197644b710ed3650fc301ea474286f5a5
Swathi-Shetty-gif <swathishetty9924@gmail.com>
2020-10-14 15:45:30 +0530

    added: firstname

--- registration.sh
+++ registration.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 echo "user registration system ensureing all validation "
+read -p "enter a first name" fname
+pat='^([A-Z]{1}[a-z]{2,})+$'
+if [ $fname =~ $pat ]
+then
+echo "valid"
+else
+ echo "not valid "
+fi
