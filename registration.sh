491e924f25e5283dd299301a2eea6c5074b64e7d
Swathi-Shetty-gif <swathishetty9924@gmail.com>
2020-10-14 16:28:58 +0530

    added : last name1

--- registration.sh
+++ registration.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "user registration system ensureing all validation "
-read -p "enter a first name" fname
-pat='^([A-Z]{1}[a-z]{2,})+$'
-if [ $fname =~ $pat ]
+read -p "enter a last name" lname
+pat='^([A-Z]{1}[a-z]{2,})+.([A-Z]{1}[a-z]{2,})+$'
+if [ $lname =~ $pat ]
 then
 echo "valid"
 else
