4967858ea867f80b3ac04685ddab2c1eb6205968
Swathi-Shetty-gif <swathishetty9924@gmail.com>
2020-10-14 18:40:34 +0530

    added: mobile number

--- registration.sh
+++ registration.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 echo "user registration system ensureing all validation "
-read -p "enter a email " email
-pat='^[a-zA-Z0-9]+([._+][a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-z]{2,3}([.][a-z]{2})*$'
-if [ $email =~ $pat ]
+read -p "enter a mobile number " mob
+pat='^(([[0-9]{2})+[ ]([0-9]{10})+)$'
+if [ $mob =~ $pat ]
 then
 echo "valid"
 else
  echo "not valid "
 fi
-#^[a-zA-Z0-9]+([.+-_*][a-zA-Z0-]+)*@[a-zA-Z0-9]+[a-z]{2,4}([.][a-z]{2})*$
